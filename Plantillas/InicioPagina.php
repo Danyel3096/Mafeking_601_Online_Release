9073c47ee4eff4c62dc3fc85fefe4d59d043ef3b
Danyel3096 <43056644+Danyel3096@users.noreply.github.com>
2019-04-11 16:27:04 -0500

    Update InicioPagina.php

--- Plantillas/InicioPagina.php
+++ Plantillas/InicioPagina.php
@@ -8,7 +8,7 @@ include_once 'App/Controladores/ControlSesion.php';
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php
 	if (!isset($Titulo) || empty($Titulo)) {
