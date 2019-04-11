af982679dee73d148520720b68ad80fb2d67e4ab
Danyel3096 <43056644+Danyel3096@users.noreply.github.com>
2019-04-11 16:39:17 -0500

    Update GestorEventoCalendario.php

--- Plantillas/Gestores/GestorEvento/GestorEventoCalendario.php
+++ Plantillas/Gestores/GestorEvento/GestorEventoCalendario.php
@@ -62,7 +62,7 @@ Conexion :: abrirConexion();
         <div class="col"></div>
     </div>
 </div>
-<div class="modal fade" id="modal-gestor-evento" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal-gestor-evento" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
